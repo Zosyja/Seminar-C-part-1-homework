75eee66fb2f0b9a70d88a647fe08cbadcd03111f
Zosyja <zosyja@bk.ru>
2023-01-19 00:52:40 +0300

    added Example 010 Third digit

---
+++ Example010_ThirdDigit/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter a number: ");
+int x = int.Parse(Console.ReadLine());
+if (x < 100)
+{
+    Console.Write("The third digit of the number IS OUT (отсутствует)");
+}
+else
+{
+    while (x > 999)
+    {
+        x = x/10;
+    }
+    int z = x%10;
+    Console.Write("The third digit of the number = ");
+    Console.Write(z);
+}
