5ed628616df5f92a337d0567021bb837019cdf3e
Zosyja <zosyja@bk.ru>
2023-01-19 00:03:41 +0300

    added Example 006 Second number

---
+++ Example006_SecondNumber/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter a three-digit number: ");
+int x = int.Parse(Console.ReadLine());
+int y = x%10; //остаток от деления на 10
+int z = x%100; //остаток от деления на 100
+int r = (z - y)/10;
+Console.Write(r);
