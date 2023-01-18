ee3a716d5aeeed38921853f99c316cf4eba2a692
Zosyja <zosyja@bk.ru>
2023-01-18 23:32:59 +0300

    added Example 002 Day of week

---
+++ Example002_DayWeek/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Enter number day of week (from 1 to 7): ");
+int x = int.Parse(Console.ReadLine());
+
+if (x==1)
+{
+    Console.Write("Monday (понедельник)");
+}
+else if(x==2)
+{
+    Console.Write("Tuesday (вторник)");
+}
+else if(x==3)
+{
+    Console.Write("Wednesday (среда)");
+}
+else if(x==4)
+{
+    Console.Write("Thursday (четверг)");
+}
+else if(x==5)
+{
+    Console.Write("Friday (пятница)");
+}
+else if(x==6)
+{
+    Console.Write("Saturday (суббота)");
+}
+else
+{
+    Console.Write("Sunday (воскресенье)");
+}
