90611e1f9489458d29663933b467f3703714466c
Zosyja <zosyja@bk.ru>
2023-01-18 23:43:58 +0300

    added Example 003 Even number

---
+++ Example003_EvenNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter a number: ");
+int x = int.Parse(Console.ReadLine());
+int r = x%2; //остаток от деления на 2
+
+if (r==1)
+{
+    Console.Write("The number is odd (число нечетное)");
+}
+else
+{
+    Console.Write("The number is even (Число четное)");
+}
