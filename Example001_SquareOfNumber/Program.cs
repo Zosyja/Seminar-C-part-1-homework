f7bc5daf5bc433f7eb4d32513377d546bba22f3b
Zosyja <zosyja@bk.ru>
2023-01-18 23:14:34 +0300

    added Example 001 Square of number

---
+++ Example001_SquareOfNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter a number1: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Enter a number2: ");
+int y = int.Parse(Console.ReadLine());
+int square = y * y;
+if (square == x)
+{
+    Console.Write("The first number IS the square of the second");
+}
+else
+{
+     Console.Write("The first number ISN't the square of the second");
+}
