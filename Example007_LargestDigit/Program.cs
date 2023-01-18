ef83860a49143755223ec6eed04926b884b45bfd
Zosyja <zosyja@bk.ru>
2023-01-19 00:13:02 +0300

    added Example 007 Largest digit

---
+++ Example007_LargestDigit/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter a number from 10 to 99: ");
+int x = int.Parse(Console.ReadLine());
+int y = x%10; //остаток от деления на 10, второе число
+int z = (x-y)/10; //первое число
+if (z>y)
+{
+    Console.Write("The max number is = ");
+    Console.Write(z);
+}
+else
+{
+    Console.Write("The max number is = ");
+    Console.Write(y);
+}
