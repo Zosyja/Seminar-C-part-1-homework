eae3cff5e157820dc15b887d304fefaadfdc08d8
Zosyja <zosyja@bk.ru>
2023-01-19 00:21:20 +0300

    added Example 008 Delete digit

---
+++ Example008_DeleteDigit/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter a three-digit number: ");
+int x = int.Parse(Console.ReadLine());
+int c = x%10; //остаток от деления на 10, третья цифра
+int z = x%100; //остаток от деления на 100
+int b = (z - c)/10; //вторая цифра
+int a = (x - z)/100; //первая цифра
+Console.Write(a); 
+Console.Write(c); 
