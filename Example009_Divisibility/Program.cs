2efa57309fdca59e04b6126899239caeeb0a7836
Zosyja <zosyja@bk.ru>
2023-01-19 00:38:18 +0300

    added Example 009 Divisibility

---
+++ Example009_Divisibility/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter a first number (divisible, делимое): ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Enter a second number (divider, делитель): ");
+int y = int.Parse(Console.ReadLine());
+int r = x%y;
+
+if (r == 0)
+{
+    Console.Write("The number IS a multiple of the entered number (кратно)");
+}
+else
+{
+    Console.WriteLine("The number ISN't a multiple of the entered number (некратно)");
+    Console.Write("The remainder of the division = ");
+    Console.Write(r); 
+}
